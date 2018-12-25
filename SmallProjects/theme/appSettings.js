1333a6043fadaf0db76d455f92931e7873588ef4
Julián Andrés <soft.electry@gmail.com>
2018-12-25 13:09:45 -0500

    Initial Commit

---
+++ SmallProjects/theme/appSettings.js
@@ -0,0 +1,10 @@
+theApp.settings.enableProfiling=false
+theApp.settings.enableBookmark=true
+theApp.settings.autoSyncContentWithView=true
+theApp.settings.showIndex=true
+theApp.settings.showGlossary=true
+theApp.settings.showBanner=true
+theApp.feedbackURL="mailto:rupfeed@us.ibm.com?subject=\"Feedback: RUP7.0.1 on RMC 7.2 (RUP for Small Projects - es)\""
+theApp.configurationTitle="RUP for Small Projects.es"
+theApp.infoFileName="about.htm"
+theApp.bannerFileName="rup_banner.jpg"
